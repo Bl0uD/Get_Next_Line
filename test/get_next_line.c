73f22780479c6c432203ed7c8f476e07a46ab557
Julien <ju@MacBook-Air-de-Ju.local>
2025-01-03 17:07:48 +0100

    update test2

--- test/get_next_line.c
+++ test/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jdupuis <jdupuis@student.42perpignan.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/02 20:18:21 by jdupuis           #+#    #+#             */
-/*   Updated: 2024/12/26 16:18:25 by jdupuis          ###   ########.fr       */
+/*   Updated: 2024/12/26 16:19:38 by jdupuis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ void	ft_bzero(void *s, size_t n)
 	while (n--)
 		*ptr++ = 0;
 }
-/*
+
 char	*get_next_line(int fd)
 {
 	char			*line;
@@ -104,72 +104,4 @@ char	*get_next_line(int fd)
 	while (nline_idx == -1 && buf_size != 0);
 	line[line_size] = '\0';
 	return (line);
-}
-*/
-
-char    *process_static_buf(char *buf, size_t *buf_size, int nline_idx)
-{
-    char    *line = malloc(nline_idx + 2);
-    if (!line)
-        return (NULL);
-    ft_memcpy(line, buf, nline_idx + 1);
-    ft_memcpy(buf, buf + nline_idx + 1, *buf_size - nline_idx - 1);
-    ft_bzero(buf + *buf_size - nline_idx - 1, BUFFER_SIZE - *buf_size + 1);
-    *buf_size -= (nline_idx + 1);
-    line[nline_idx + 1] = '\0';
-    return (line);
-}
-
-char    *append_and_finalize(char *buf, size_t *buf_size, char *line, int *line_size, int nline_idx, ssize_t r)
-{
-    if (nline_idx != -1)
-    {
-        line = ft_join(line, *line_size, buf, nline_idx + 1);
-        if (!line)
-            return (NULL);
-        *line_size += nline_idx + 1;
-        ft_memcpy(buf, buf + nline_idx + 1, r - nline_idx - 1);
-        ft_bzero(buf + (r - nline_idx - 1), BUFFER_SIZE - r + nline_idx + 1);
-        *buf_size = r - nline_idx - 1;
-        line[*line_size] = '\0';
-        return (line);
-    }
-    line = ft_join(line, *line_size, buf, r);
-    if (!line)
-        return (NULL);
-    line[*line_size + r] = '\0';
-    *buf_size = 0;
-    return (line);
-}
-
-char    *get_next_line(int fd)
-{
-    static char buf[BUFFER_SIZE] = {0};
-    static size_t   buf_size = 0;
-    char    *line;
-    ssize_t r;
-    int nline_idx, line_size;
-
-    line = NULL;
-    nline_idx = find_newline(buf, buf_size);
-    if (nline_idx != -1)
-        return (process_static_buf(buf, &buf_size, nline_idx));
-    line = malloc(buf_size + 1);
-    if (!line)
-        return (NULL);
-    ft_memcpy(line, buf, buf_size);
-    line_size = buf_size;
-    while ((r = read(fd, buf, BUFFER_SIZE)) > 0)
-    {
-        buf_size = r;
-        nline_idx = find_newline(buf, buf_size);
-        if (nline_idx != -1 || buf_size < BUFFER_SIZE)
-            return (append_and_finalize(buf, &buf_size, line, &line_size, nline_idx, r));
-        line = ft_join(line, line_size, buf, buf_size);
-        if (!line)
-            return (NULL);
-        line_size += buf_size;
-    }
-    free(line);
-    return (NULL);
 }
---
+++ test2/get_next_line.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jdupuis <jdupuis@student.42perpignan.fr    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/27 16:10:33 by jdupuis           #+#    #+#             */
+/*   Updated: 2025/01/03 16:56:33 by jdupuis          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
+
+size_t			ft_strlen(char *str);
+char			*ft_strchr(char *s, int c);
+char			*ft_strjoin(char *s1, char *s2);
+char			*get_next_line(int fd);
+
+char			*ft_get_line(char *save);
+char			*ft_save(char *save);
+char			*ft_read_and_save(int fd, char *save);
+
+#endif
